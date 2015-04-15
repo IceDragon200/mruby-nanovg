90d252177280392805f5e1d4ee6e5d05de635cea
Corey Powell <mistdragon100@gmail.com>
2015-04-15 09:40:52 -0500

    Added Nanovg::Color#set

--- mrblib/color.rb
+++ mrblib/color.rb
@@ -1,5 +1,20 @@
 module Nanovg
   class Color
+    # Set all channels in the Color at once
+    #
+    # @param [Float] r
+    # @param [Float] g
+    # @param [Float] b
+    # @param [Float] a
+    # @return [self]
+    #
+    # @example
+    #   color.set(1, 1, 0, 0.5)
+    def set(r, g, b, a)
+      self.r, self.g, self.b, self.a = r, g, b, a
+      self
+    end
+
     def self.mono(value)
       rgb(value, value, value)
     end
